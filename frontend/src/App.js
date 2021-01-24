a36600d3ee1d96144a674d8aad2b9ff57e24471c
thiago jose da silva <thiago.ads.silva@gmail.com>
2021-01-23 21:39:24 -0300

    Criaçao das routas no arquivo APP.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import {useRouter} from 'hookrouter';
+
+import HomeScreen from './components/HomeScreen';
+import ListTask from './components/ListTask';
+import RegisterTask from './components/RegisterTask';
+import UpdateTask from './components/UpdateTask';
+
+const routes = {
+  '/': () => <HomeScreen />,
+  '/listTask': () => <ListTask />,
+  '/registerTask': () => <RegisterTask />,
+  '/updateTask': () => <UpdateTask />
+}
+
 function App() {
-  return (
-    <div>
-      Thiago jose da silva
-    </div>
-  );
+  return useRouter(routes);
 }
 
 export default App;
