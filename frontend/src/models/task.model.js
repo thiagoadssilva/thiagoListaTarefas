5c0589e4855822643bd43bc045f960d5230ce7be
thiago jose da silva <thiago.ads.silva@gmail.com>
2021-01-23 21:53:46 -0300

    Criado o arquivo model das tarefas

---
+++ frontend/src/models/task.model.js
@@ -0,0 +1,13 @@
+function Task(id, name, concluded){
+    this.id = id;
+    this.name = name;
+    this.concluded = concluded;
+    
+    return{
+        id: id,
+        name: name,
+        concluded: concluded
+    };
+}
+
+export default Task;
